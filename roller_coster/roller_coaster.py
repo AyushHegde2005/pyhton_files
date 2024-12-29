d31291fb2687530a7e1c7b6475d54462c21859f2
ayushhegde2005 <ayush16.mitmpl2023@learner.manipal.edu>
2024-12-29 12:34:16 +0000

    modified the roller_coaster code

--- roller_coster/roller_coaster.py
+++ roller_coster/roller_coaster.py
@@ -5,11 +5,14 @@ height = int(input("Please enter your height in cm :"))
 if height >= 120:
     print("You are eligible to ride the roller coaster.")
     age = int(input("Enter your age:"))
-    if age >= 18:
-        print("The cost of your ticket will be Rs.150")
-    else:
+    if 12 > age:
+        print("The cost of your ticket will be Rs.100")
+    elif 18 >= age:
         print("The cost of your ticket will be Rs.120")
+    else:
+        print("The cost of your ticket will be Rs.150")
 
 else:
     print("Sorry, but you are not tall enough to ride the roller coaster.")
 
+print("Thank You!!")
