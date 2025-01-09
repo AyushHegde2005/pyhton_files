dc568dd974ae59aaefc17bb538da2c471bb2b299
ayushhegde2005 <ayush16.mitmpl2023@learner.manipal.edu>
2025-01-09 10:33:33 +0000

    changed few lines of the program

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,40 +1,43 @@
 import random
 
-choice = int(input("What do you choose? Type 0 for rock, 1 for paper and 2 for scissors."))
-print("You choose:")
-if 0 == choice:
-    print('''
+
+rock = '''
         _______
     ---'   ____)
           (_____)
           (_____)
           (____)
     ---.__(___)
-
     rock
-    ''')
-elif 1 == choice:
-    print('''
+    '''
+paper = '''
          _______
      ---'   ____)____
                ______)
                _______)
               _______)
      ---.__________)
-    
      paper
-    ''')
+     '''      
+scissors = '''
+          _______
+      ---'   ____)____
+                ______)
+             __________)
+            (____)
+      ---.__(___)
+      scissors
+      '''
+
+
+choice = int(input("What do you choose? Type 0 for rock, 1 for paper and 2 for scissors."))
+print("You choose:")
+if 0 == choice:
+    print(rock)
+elif 1 == choice:
+    print(paper)
 elif 2 == choice:
-    print('''
-        _______
-    ---'   ____)____
-              ______)
-           __________)
-          (____)
-    ---.__(___)
-     
-     scissors
-    ''')
+    print(scissors)
 else:
     print("You have typed wrong number.")
 
@@ -42,39 +45,12 @@ if 0 == choice or 1 == choice or 2 == choice:
     comp_choice = random.randint(0, 2)
     print("computer choose:")
     if 0 == comp_choice:
-        print('''
-            _______
-        ---'   ____)
-              (_____)
-              (_____)
-              (____)
-        ---.__(___)
-
-        rock
-        ''')
+        print(rock)
     elif 1 == comp_choice:
-        print('''
-            _______
-        ---'   ____)____
-                  ______)
-                  _______)
-                _______)
-        ---.__________)
-
-        paper
-        ''')
+        print(paper)
 
     elif 2 == comp_choice:
-        print('''
-            _______
-        ---'   ____)____
-                  ______)
-               __________)
-              (____)
-        ---.__(___)
-
-        scissors
-          ''')
+        print(scissors)
 
     if choice == comp_choice:
         print("It's a draw!")
