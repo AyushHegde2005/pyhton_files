336f9ed3fd66aaac47189bc48c00942234e4dab5
ayushhegde2005 <ayush16.mitmpl2023@learner.manipal.edu>
2025-01-12 08:14:22 +0000

    added .lower() function to make the code even efficient

--- python_pizza_delivery_program/python_pizza_delivery_program.py
+++ python_pizza_delivery_program/python_pizza_delivery_program.py
@@ -1,21 +1,22 @@
+import string
 print("Welcome to python pizza deliveries!")
-print("Would you like to order a pizza?").lower
-if 'y' == size:
-    size = input("What size of pizza do you want? S(small), M(medium), or L(large):")
-    if 's' == size or 'S' == size:
+choice = input("Would you like to order a pizza?\n").lower()
+if "y" == choice:
+    size = input("What size of pizza do you want? S(small), M(medium), or L(large):\n").lower()
+    if 's' == size:
         bill = 199
-    elif 'm' == size or 'm' == size:
+    elif 'm' == size:
         bill = 299
     else:
         bill = 399
 
-    extra_cheese = input("Do you want extra cheese? Y or N:")
-    if 'y' == extra_cheese or 'Y' == extra_cheese:
+    extra_cheese = input("Do you want extra cheese? Y or N:\n").lower()
+    if 'y' == extra_cheese:
         bill += 20
 
-    peperoni = input("Do you want peperoni? Y or n N:")
-    if 'y'== peperoni or 'Y' == peperoni:
-        if 's' == size or 'S' == size:
+    peperoni = input("Do you want peperoni? Y or n N:\n").lower()
+    if 'y'== peperoni:
+        if 's' == size:
             bill += 40
         else:
             bill += 50
@@ -23,7 +24,7 @@ if 'y' == size:
     print(f"\nYour choice of for pizza is as follow")
     print(f"Pizza=={size}\nextra cheese=={extra_cheese}\npeperoni=={peperoni}")
     print(f"The total bill price if Rs.{bill}")
-    print("Thank you, please visit us again!"
+    print("Thank you, please visit us again!")
 
 else:
     print("sure no problem, thank you!")
